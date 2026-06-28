7d53d258843cdc902bcad559c5874bbad90440ee
Dhruv tomar <dhruvtomar2006@gmail.com>
2026-06-28 21:12:17 +0530

    Add files via upload

---
+++ codechef/24jun/2nd que.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+            int n, k;
+        cin >> n >> k;
+            int player = 1; 
+            
+         while (player + k <= n) {
+            player = player + k;
+        }
+        cout << player << endl;
+    }
+    return 0;
+}
